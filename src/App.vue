5e462557e08d5b79857fca51dea306090c657f4a
thalals <thalsal@naver.com>
2024-07-31 23:18:41 +0900

    chore : router 구조 설정 및 부트스트랩 vue install

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,11 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="@/assets/falling_logo.png" width="125" height="125" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">Home</RouterLink> |
+        <RouterLink to="/grid-system">About</RouterLink>
       </nav>
 
 
---
+++ src/views/GridSystemView.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+<div>
+  Grid System
+</div>
+</template>
+
+<style scoped>
+
+</style>
