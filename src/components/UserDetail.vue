99f62af448530293befcea7703cbf3b3d5a3c818
thalals <thalsal@naver.com>
2024-08-11 00:39:19 +0900

    feat : 유저 디테일 정보 조회 레이아웃

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -17,29 +17,27 @@
 
         <div class="detail-info">
 
-            <span> <b-badge variant="light">전화번호</b-badge> : {{ userDetailData.phoneNumber }}</span> <br>
-            <span> <b-badge variant="light">닉네임</b-badge> : {{ userDetailData.username }}</span> <br>
-            <span> <b-badge variant="light">생년 월일</b-badge> : {{ userDetailData.birthDay }}</span> <br>
-            <span> <b-badge variant="light">이메일</b-badge> : {{ userDetailData.email }}</span> <br>
-            <span> <b-badge variant="light">거주지</b-badge> : {{ userDetailData.userLocation }}</span> <br>
-            <span> <b-badge variant="light">키</b-badge> : {{ userDetailData.tall }} cm</span> <br>
-            <span> <b-badge variant="light">성별</b-badge> : {{ userDetailData.gender }}</span> <br>
-            <span> <b-badge variant="light">선호 성별</b-badge> : {{ userDetailData.preferGender }}</span> <br>
-            <span> <b-badge variant="light">음주 여부</b-badge> : {{ userDetailData.drinkStatus }}</span> <br>
-            <span> <b-badge variant="light">흡연 여부</b-badge> : {{ userDetailData.smokingStatus }}</span> <br>
-            <span> <b-badge variant="light">종교</b-badge> : {{ userDetailData.religion }}</span> <br>
+            <span> <b-badge variant="success">전화번호</b-badge> : {{ userDetailData.phoneNumber }}</span> <br>
+            <span> <b-badge variant="success">닉네임</b-badge> : {{ userDetailData.username }}</span> <br>
+            <span> <b-badge variant="success">생년 월일</b-badge> : {{ userDetailData.birthDay }}</span> <br>
+            <span> <b-badge variant="success">이메일</b-badge> : {{ userDetailData.email }}</span> <br>
+            <span> <b-badge variant="success">거주지</b-badge> : {{ userDetailData.userLocation }}</span> <br>
+            <span> <b-badge variant="success">키</b-badge> : {{ userDetailData.tall }} cm</span> <br>
+            <span> <b-badge variant="success">성별</b-badge> : {{ userDetailData.gender }}</span> <br>
+            <span> <b-badge variant="success">선호 성별</b-badge> : {{ userDetailData.preferGender }}</span> <br>
+            <span> <b-badge variant="success">음주 여부</b-badge> : {{ userDetailData.drinkStatus }}</span> <br>
+            <span> <b-badge variant="success">흡연 여부</b-badge> : {{ userDetailData.smokingStatus }}</span> <br>
+            <span> <b-badge variant="success">종교</b-badge> : {{ userDetailData.religion }}</span> <br>
         </div>
         <div class="detail-info">
-            <span> <b-badge variant="light">서비스 이용약관 동의 목록</b-badge> <br>
-                {{ userDetailData.serviceAgreeList }}
-<!--                <span v-for="([agreeCategory, isAgree], index) in userDetailData.serviceAgreeList">-->
-<!--                    {{ agreeCategory }} : {{ isAgree ? "동의" : "비동의"}}-->
-<!--                </span>-->
-
+            <span> <b-badge variant="success">서비스 이용약관 동의 목록</b-badge> <br>
+                <span v-for="(isAgree, agreeCategory) in userDetailData.serviceAgreeList">
+                    ✔️ {{ agreeCategory }} : {{ isAgree ? " 동의 " : " 비동의 " }} <br>
+                </span>
             </span> <br>
-            <span> <b-badge variant="light">SNS 가입 타입</b-badge> : {{ userDetailData.snsSignUpList }}</span> <br>
-            <span> <b-badge variant="light">관심사</b-badge> : {{ userDetailData.interests }}</span> <br>
-            <span> <b-badge variant="light">이상형</b-badge> : {{ userDetailData.idealTypes }}</span> <br>
+            <span> <b-badge variant="success">SNS 가입 타입</b-badge> : {{ userDetailData.snsSignUpList }}</span> <br>
+            <span> <b-badge variant="success">관심사</b-badge> : {{ userDetailData.interests }}</span> <br>
+            <span> <b-badge variant="success">이상형</b-badge> : {{ userDetailData.idealTypes }}</span> <br>
 
         </div>
     </div>
